ceb68f3c8bcd94d7c60d9445af4494608480b083
francolargo <frankw@umn.edu>
2016-01-25 09:04:19 -0700

    Update netio_server.py

--- netio_server.py
+++ netio_server.py
@@ -5,16 +5,40 @@ import socket
 import select
 import Adafruit_BBIO.GPIO as GPIO
 import time
-import subprocess  # will be used in the future to call 'subroutine' scripts
+import subprocess
 import smbus
-import os
+#import os
 
 # misc. GPIO setup as needed to address pins
-#GPIO.setup("P8_19", GPIO.OUT)
-#GPIO.output("P8_19", GPIO.LOW)
-#GPIO.setup("P8_18", GPIO.IN)
-# volume setup
-SWITCH1 = "P8_18"
+GPIO.setup("P8_7", GPIO.OUT) # goes to Otto S input
+GPIO.output("P8_7", GPIO.LOW)
+GPIO.setup("P8_11", GPIO.IN)
+GPIO.add_event_detect("P8_11", GPIO.RISING)
+GPIO.setup("P8_12", GPIO.IN)
+GPIO.add_event_detect("P8_12", GPIO.RISING)
+GPIO.setup("P8_14", GPIO.IN)
+GPIO.add_event_detect("P8_14", GPIO.RISING)
+GPIO.setup("P8_16", GPIO.IN)
+GPIO.add_event_detect("P8_16", GPIO.RISING)
+GPIO.setup("P8_18", GPIO.IN)
+GPIO.add_event_detect("P8_18", GPIO.RISING)
+GPIO.setup("P8_9", GPIO.OUT) # goes to Otto OE input
+GPIO.output("P8_9", GPIO.HIGH)
+GPIO.setup("P8_10", GPIO.OUT)
+GPIO.output("P8_10", GPIO.LOW)
+GPIO.setup("P8_13", GPIO.OUT)
+GPIO.output("P8_13", GPIO.LOW)
+GPIO.setup("P8_15", GPIO.OUT)
+GPIO.output("P8_15", GPIO.LOW)
+GPIO.setup("P8_17", GPIO.OUT)
+GPIO.output("P8_17", GPIO.LOW)
+GPIO.setup("P8_19", GPIO.OUT)
+GPIO.output("P8_19", GPIO.LOW)
+# initialize for BBB -> speakers
+subprocess.call("./BBB_in.sh")
+subprocess.call("./Speaker_out.sh")
+# volume & setup
+#SWITCH1 = "P8_18"
 LIGHT = 0
 volume = 10
 tweeter = 0
@@ -23,7 +47,7 @@ woofer = 0
 rate = 0
 rawrate = 0
 bus = smbus.SMBus(1)
-mutebus = 206
+mutebus = 207
 # setup I2C volume control
 try:
    bus.write_byte_data(0x70, 0x40, 0x06)
@@ -39,6 +63,7 @@ except IOError as err:
    print "DAC turned on?"
 else:
    print "DAC ready"
+#os.spawnl(os.P_DETACH, "./buttons.py")
 class Client(asyncore.dispatcher_with_send):
     def __init__(self, socket=None, pollster=None):
         asyncore.dispatcher_with_send.__init__(self, socket)
@@ -52,7 +77,7 @@ class Client(asyncore.dispatcher_with_send):
             self.pollster.unregister(self)
 
     def handle_read(self):
-        receivedData = self.recv(8192)
+        receivedData = self.recv(8192) #8192 = buffer size
         if not receivedData:
             self.close()
             return
@@ -97,9 +122,9 @@ class Client(asyncore.dispatcher_with_send):
 	    value = int(line[10:13])
         # print(value)
 	    volume = value
-            set6 = volume + midrange
-            set5 = volume + tweeter
-            set4 = volume + woofer
+            set6 = volume + int(midrange * volume / 100)
+            set5 = volume + int(woofer * volume / 100)
+            set4 = volume + int(tweeter * volume / 100)
             bus.write_byte_data(0x70, 0x40, 0x06)
             bus.write_byte_data(0x48, 0x17, set6)
             bus.write_byte_data(0x70, 0x40, 0x05)
@@ -110,23 +135,23 @@ class Client(asyncore.dispatcher_with_send):
         elif line.startswith("tweeter"):
             tweeter = int(line[8:11])
             # print(tweeter)
-            set5 = volume + tweeter
-            bus.write_byte_data(0x70, 0x40, 0x05)
-            bus.write_byte_data(0x48, 0x17, set5)
+            set4 = volume + int(tweeter * volume / 100)
+            bus.write_byte_data(0x70, 0x40, 0x04)
+            bus.write_byte_data(0x48, 0x17, set4)
             self.send("ok\n")
         elif line.startswith("midrange"):
             midrange = int(line[9:12])
             # print(midrange)
-            set6 = volume + midrange
+            set6 = volume + int(midrange * volume / 100)
             bus.write_byte_data(0x70, 0x40, 0x06)
             bus.write_byte_data(0x48, 0x17, set6)
             self.send("ok\n")
         elif line.startswith("woofer"):
             woofer = int(line[7:10])
             # print(woofer)
-            set4 = volume + woofer
-            bus.write_byte_data(0x70, 0x40, 0x04)
-            bus.write_byte_data(0x48, 0x17, set4)
+            set5 = volume + int(woofer * volume / 100)
+            bus.write_byte_data(0x70, 0x40, 0x05)
+            bus.write_byte_data(0x48, 0x17, set5)
             self.send("ok\n")
         elif line.startswith("balance"):
             balance = int(line[8:11])
@@ -250,7 +275,6 @@ def readwrite(obj, flags):
     except:
         obj.handle_error()
 
-
 class EPoll(object):
     def __init__(self):
         self.epoll = select.epoll()
@@ -274,6 +298,7 @@ if __name__ == "__main__":
     pollster.register(Server(("",8192),pollster), select.EPOLLIN)
     pollster.register(Server(("",8193),pollster), select.EPOLLIN)
     while True:
+        #import ipdb; ipdb.set_trace()
         evt = pollster.poll()
         for obj, flags in evt:
             readwrite(obj, flags)
